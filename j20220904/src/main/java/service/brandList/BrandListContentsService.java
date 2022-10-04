a3ef1ae0e173f39861c35a72731bbcb0201478a4
YongQ <cyk728@naver.com>
2022-10-04 16:20:10 +0900

    브랜드 리스트 페이지 추가

--- j20220904/src/main/java/service/brandList/BrandListContentsService.java
+++ j20220904/src/main/java/service/brandList/BrandListContentsService.java
@@ -16,42 +16,44 @@ public class BrandListContentsService implements CommandProcess {
 	@Override
 	public String requestPro(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		
 		BrandListDAO bld = BrandListDAO.getInstance();
 		
 		int ca_code = Integer.parseInt(request.getParameter("ca_code"));
 		
+		
 		try {
-			List<Product_ImgSrcDTO> list = bld.selectCa_code(ca_code);
-			int totCnt = bld.getTotalCnt(); // 14개
+//			int totCnt = bld.getTotalCnt(ca_code); // 14개
+//			
+//			String pageNum = request.getParameter("pageNum");
+//			if (pageNum == null || pageNum.equals("")) {pageNum = "1";}
+//			int currentPage = Integer.parseInt(pageNum);
+//			int pageSize = 10, blockSize = 12;
+//			int startRow = (currentPage - 1) * pageSize + 1;
+//			int endRow = startRow + pageSize - 1;
+//			int startNum = totCnt - startRow + 1;
+			String ca_name = bld.selectBrandName(ca_code);
+			List<Product_ImgSrcDTO> list = bld.productList(ca_code);
 			
-			String pageNum = request.getParameter("pageNum");
-			if (pageNum == null || pageNum.equals("")) {pageNum = "1";}
-			int currentPage = Integer.parseInt(pageNum);
-			int pageSize = 10, blockSize = 12;
-			int startRow = (currentPage - 1) * pageSize + 1;
-			int endRow = startRow + pageSize - 1;
-			int startNum = totCnt - startRow + 1;
-			
-			List<Product_ImgSrcDTO> list1 = bld.productList(startRow, endRow);
-			System.out.println("list" + list1);
-			
-			int pageCnt = (int)Math.ceil((double)totCnt/pageSize);
-			
-			int startPage = (int)(currentPage - 1)/blockSize * blockSize + 1;
-			int endPage = startPage + blockSize - 1;
-			if (endPage > pageCnt) endPage = pageCnt;
-			
+			System.out.println("list" + list);
+			System.out.println(ca_name);
+//			int pageCnt = (int)Math.ceil((double)totCnt/pageSize);
+//			
+//			int startPage = (int)(currentPage - 1)/blockSize * blockSize + 1;
+//			int endPage = startPage + blockSize - 1;
+//			if (endPage > pageCnt) endPage = pageCnt;
+//			
+			request.setAttribute("ca_name", ca_name);
 			request.setAttribute("list", list);
-			request.setAttribute("list1", list1);
+//			request.setAttribute("totCnt", totCnt);
+//			request.setAttribute("pageNum", pageNum);
+//			request.setAttribute("currentPage", currentPage);
+//			request.setAttribute("startNum", startNum);
+//			request.setAttribute("blockSize", blockSize);
+//			request.setAttribute("pageCnt", pageCnt);
+//			request.setAttribute("startPage", startPage);
+//			request.setAttribute("endPage", endPage);
 			
-			request.setAttribute("totCnt", totCnt);
-			request.setAttribute("pageNum", pageNum);
-			request.setAttribute("currentPage", currentPage);
-			request.setAttribute("startNum", startNum);
-			request.setAttribute("blockSize", blockSize);
-			request.setAttribute("pageCnt", pageCnt);
-			request.setAttribute("startPage", startPage);
-			request.setAttribute("endPage", endPage);
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
