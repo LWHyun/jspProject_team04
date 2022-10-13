0dbb61ad3e193c55b43103352adf413d962971e7
zest_ <zest_@DESKTOP-Q3CUEGA>
2022-10-13 19:18:53 +0900

    카테고리 최종 에러 해결 및 로그인시 메인페이지말고 보던 페이지로 가게

--- j20220904/src/main/java/service/category/WomenService.java
+++ j20220904/src/main/java/service/category/WomenService.java
@@ -22,6 +22,8 @@ public class WomenService implements CommandProcess {
 		//카테고리탭에서 여성용탭에 해당하는 동작
 		
 		int gender = Integer.parseInt(request.getParameter("gender"));
+		String toURI = request.getRequestURI();
+		toURI = toURI+"?gender="+gender;
 		String result = null;
 		if(gender == 0) {
 			result = "남성용";
@@ -38,7 +40,7 @@ public class WomenService implements CommandProcess {
 			List<Product_ImgSrcDTO> list = cd.selectSearch(gender,mem_id);
 			request.setAttribute("result", result);
 			request.setAttribute("list", list);
-			
+			request.setAttribute("toURI", toURI);
 			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
